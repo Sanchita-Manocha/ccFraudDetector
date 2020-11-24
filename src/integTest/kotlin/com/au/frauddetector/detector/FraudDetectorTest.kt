fb41d22643b5d997fd76858182e287c37ee11a0b
sanchitam <sanchita.manocha@afterpaytouch.com>
2020-11-24 17:12:04 +1100

    Add configration file for fraud detector

---
+++ src/integTest/kotlin/com/au/frauddetector/detector/FraudDetectorTest.kt
@@ -0,0 +1,5 @@
+package com.au.frauddetector.detector
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class FraudDetectorTest
---
+++ src/main/kotlin/com/au/frauddetector/config/Config.kt
@@ -0,0 +1,21 @@
+package com.au.frauddetector.config
+
+import java.math.BigDecimal
+
+data class Config(
+    val priceThreshold: BigDecimal,
+    val slidingTimeWindowInHours: Int,
+    val sourceFile: String,
+) {
+    companion object {
+        fun defaultConfig() {
+            Config(
+                env("PRICE_THRESHOLD", "100.00").toBigDecimal(),
+                env("TIME_WINDOW", "24").toInt(),
+                env("SOURCE_FILE", "transaction.csv"),
+            )
+        }
+    }
+}
+
+private fun env(name: String, default: String): String = System.getenv(name) ?: default
