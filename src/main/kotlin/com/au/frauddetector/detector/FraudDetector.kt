21ee1f80701b53e386467b5c403ff91229604490
sanchitam <sanchita.manocha@afterpaytouch.com>
2020-11-27 16:30:07 +1100

    Fix stream closed error

--- src/main/kotlin/com/au/frauddetector/detector/FraudDetector.kt
+++ src/main/kotlin/com/au/frauddetector/detector/FraudDetector.kt
@@ -5,24 +5,29 @@ import com.au.frauddetector.config.Config
 import com.au.frauddetector.domain.Transaction
 import org.slf4j.LoggerFactory
 import java.io.File
-import java.io.FileNotFoundException
 import java.net.URL
 
 class FraudDetector(private val config: Config) {
     val logger = LoggerFactory.getLogger(this::class.java)
     fun getFraudCards() {
-        readTxFile(this::class.java.classLoader.getResource(config.sourceFile))
-
+        val validTxn = readTxFile(this::class.java.classLoader.getResource(config.sourceFile))
     }
 
-    private fun readTxFile(filePath: URL?) : Sequence<Transaction> {
-        filePath ?: throw FileNotFoundException("${config.sourceFile} File not found")
-        return File(filePath.file).useLines { it}
+    private fun readTxFile(filePath: URL?): List<Transaction> {
+        return File(filePath?.file).useLines { it.toList() }
             .map { it.split(",") }
             .filter { it.size == 3 }
-            .map { Transaction(creditCardNumber = it[0], time = it[1], amount = it[2].toBigDecimal()) }.also {
+            .map {
+                Transaction(
+                    creditCardNumber = it[0],
+                    time = it[1],
+                    amount = it[2].replace("\\s".toRegex(), "").toBigDecimal()
+                )
+            }.also {
                 logger.info("Reading transactions from File - '${config.sourceFile}'")
             }
     }
-
 }
+
+
+
