1b426fe73927a2609825ad6a5b11da0103a2affe
sanchitam <sanchita.manocha@afterpaytouch.com>
2020-12-08 11:11:50 +1100

    Add integTests

--- src/integTest/kotlin/com/au/frauddetector/detector/FraudDetectorTest.kt
+++ src/integTest/kotlin/com/au/frauddetector/detector/FraudDetectorTest.kt
@@ -2,6 +2,7 @@ package com.au.frauddetector.detector
 
 import com.au.frauddetector.config.Config
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.io.FileNotFoundException
@@ -34,9 +35,19 @@ internal class FraudDetectorTest {
 
     @Test
     fun `multiple txs from a card exceeds threshold with in sliding window`() {
+        val testConfig = Config.defaultConfig().let {
+            it.copy(sourceFile = "sample_file_with_multiple_txn_total_amt_exceed_threshold.csv") }
+        val fraudCards = FraudDetector(testConfig).detectFraud()
+        assertEquals(1, fraudCards.size)
+        assertEquals("10d7ce2f43e45fa57d1bbf8b1e1", fraudCards.single())
     }
 
     @Test
-    fun `multiple txs from a card exceeds threshold but not in sliding window`() {
+    fun `multiple cards exceed threshold with in sliding window`() {
+        val testConfig = Config.defaultConfig().let {
+            it.copy(sourceFile = "sample_file_with_multiple_fraud_cards.csv") }
+        val fraudCards = FraudDetector(testConfig).detectFraud()
+        assertEquals(3, fraudCards.size)
+        assertTrue(fraudCards.containsAll(listOf("10d7ce2f43e45fa57d1bbf8b1e2", "10d7ce2f43e45fa57d1bbf8b1e3", "10d7ce2f43e45fa57d1bbf8b1e4")))
     }
 }
